a24440b4066d796544a2c879a47e4b0da9ba719c
Hritik408 <chaudharyhritik91@gmail.com>
2024-01-15 00:34:22 +0530

    accordian for menu

---
+++ src/components/RestCatagories.js
@@ -0,0 +1,24 @@
+import ItemList from "./ItemList";
+
+const RestCatagories = ({data, showItems, setshowIndex, jaat}) => {   // restCatagories is control by restMenu & if this has own state then it is uncontrol
+    //  console.log(data);
+   const handleClick = () => setshowIndex(jaat);
+ //  const handleClick = () => setshowIndex();
+
+    return (
+        <div>
+            <div className="p-3 bg-gray-200 shadow-lg mx-auto my-3 w-6/12 ">
+
+                <div className="flex justify-between cursor-pointer" onClick={handleClick}>
+                <span className="font-bold italic text-lg">{data.title} ({data.itemCards.length}) </span>
+                 <span className="justify-end">{"⬇️"}</span> 
+                </div>
+
+             {showItems && <ItemList items = {data.itemCards} />}
+            </div>
+    
+        </div>
+    )
+}
+
+export default RestCatagories;
---
+++ src/components/RestMenu.js
@@ -0,0 +1,85 @@
+import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestCatagories from "./RestCatagories";
+import { useState } from "react";
+
+
+const RestMenu = () => {
+
+     const {restId} = useParams();   // here restId is comes from app.js path/element 
+
+    const resdata = useRestaurantMenu(restId);
+
+ //   const[showIndex, setshowIndex] = useState(false);
+ //  const[showIndex, setshowIndex] = useState(true);
+   const[showIndex, setshowIndex] = useState(null);
+  
+
+    if(resdata === null)  return <Shimmer />
+    
+     const {name, cuisines, costForTwoMessage} = resdata?.cards[0]?.card?.card?.info;
+
+  //   const {itemCards} = resdata?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+
+   //  console.log(itemCards);
+     
+       const Catagories = resdata?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((c) => (
+        c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"   // if @ comes then use [""]
+       ));
+   //    console.log(Catagories);
+
+   return  (
+        <div className="text-center my-5">
+        <h1 className="font-bold text-2xl ">{name}</h1>
+        <p className="font-bold  my-2 italic">{cuisines.join(", ")} - {costForTwoMessage} </p>
+        {/* catogories accordians */}
+        {Catagories.map((catagory, index)=>(
+          // controlled component
+          <RestCatagories 
+          key={catagory?.card?.card?.title} 
+          data = {catagory?.card?.card}
+        //  jaat = {index} 
+        showItems = {index === showIndex ? true : false} 
+            setshowIndex = {(val) => setshowIndex(showIndex === value ? index : value)}
+       //     setshowIndex = {() => setshowIndex(index)}   // this is for only one open and other will be closed
+      //   setshowIndex = {(value)=> setshowIndex((value) = !value)}
+  //    setshowIndex={(val) => !val}
+         />
+        ))}
+
+        </div>
+     )
+}
+
+export default RestMenu;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ {/* <ul>
+       {itemCards.map((item)=>(
+        <li key={item.card.info.id}> {item.card.info.name} </li>
+       ))}
+        </ul> */}
